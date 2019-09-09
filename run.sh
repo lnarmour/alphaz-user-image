8c421752788d08f27a05ac24e75c9a836b511911
Louis Narmour <lnarmour@rams.colostate.edu>
2019-09-08 19:06:34 -0600

    add support for running container on both mac and linux

--- run.sh
+++ run.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
 # if MacOX
-ip=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}');
-x11="$HOME/.Xauthority:/home/developer/.Xauthority";
+if [[ "$(uname)" == "Darwin" ]]; then
+	ip=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}');
+	x11="$HOME/.Xauthority:/home/developer/.Xauthority";
+fi;
 
-# else
-#ip=$DISPLAY;
-#x11="/tmp/.X11-unix:/tmp/.X11-unix";
+# if Linux
+if [[ "$(uname)" == "Linux" ]]; then
+	ip=$DISPLAY;
+	x11="/tmp/.X11-unix:/tmp/.X11-unix";
+fi;
+
+if [[ -z "$ip" && -z "$x11" ]]; then
+	echo "Error: could not detect that this is running on MacOS or Linux";
+	exit;
+fi;
 
 docker run -ti --rm \
            -e DISPLAY=$ip:0 \
