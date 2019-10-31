34856cbcf57d2fe26ad41b3810338ade608dd9b6
Louis Narmour <lnarmour@rams.colostate.edu>
2019-10-31 07:17:47 -0600

    fix x11 forwarding to use localhost

--- run.sh
+++ run.sh
@@ -2,27 +2,25 @@
 
 # if MacOX
 if [[ "$(uname)" == "Darwin" ]]; then
-	ip=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}');
 	x11="$HOME/.Xauthority:/home/developer/.Xauthority";
 fi;
 
 # if Linux
 if [[ "$(uname)" == "Linux" ]]; then
-	ip=$DISPLAY;
 	x11="/tmp/.X11-unix:/tmp/.X11-unix";
 fi;
 
-if [[ -z "$ip" && -z "$x11" ]]; then
+if [[ -z "$x11" ]]; then
 	echo "Error: could not detect that this is running on MacOS or Linux";
 	exit;
 fi;
 
-mkdir -p $HOME/alphaz-docker/workspace $HOME/alphaz-docker/git
-
+mkdir -p $HOME/alphaz-user-docker/workspace $HOME/alphaz-user-docker/git
+xhost + 127.0.0.1;
 docker run -ti --rm \
-           -e DISPLAY=$ip:0 \
+           -e DISPLAY=host.docker.internal:0 \
            -v "$x11" \
-           -v $HOME/alphaz-docker/workspace:/home/developer/eclipse-workspace \
-           -v $HOME/alphaz-docker/git:/home/developer/git \
+           -v $HOME/alphaz-user-docker/workspace:/home/developer/eclipse-workspace \
+           -v $HOME/alphaz-user-docker/git:/home/developer/git \
            --name alphaz-user \
            narmour/alphaz-user-image:latest
